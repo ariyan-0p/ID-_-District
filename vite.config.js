b04180ba1a479fcdac3d8a4f520aa099f6f68d8f
ariyan-0p <likuariyan2002@gmail.com>
2025-09-20 14:24:55 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Add this 'base' property
-  base: '/ID---District-8/', 
+  // This path MUST match your repository name
+  base: '/ID-_-District/', 
   plugins: [react()],
 })
